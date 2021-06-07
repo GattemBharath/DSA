51b8d971d4681da556335c01fa1e60ef87d769f8
Gattem Bharath <53399475+GattemBharath@users.noreply.github.com>
2021-06-07 22:18:54 +0530

    Update Lower-Upper case.py

--- Strings/Lower-Upper case.py
+++ Strings/Lower-Upper case.py
@@ -1,7 +1,22 @@
+Given a string str containing only lowercase letters, generate a string with the same letters, but in uppercase.
+
+Example 1:
+
+Input:
+str = "geeks"
+Output: GEEKS
+Example 2:
+
+Input:
+str = "geeksforgeeks"
+Output: GEEKSFORGEEKS
+
+
+
 def to_upper(str):
     #return str.upper()
     s = ''
     for i in str:
         s += chr(ord(i)-32)
     return s
-print(to_upper('geeksforgeeks'))
+print(to_upper('geeksforgeeks'))  # GEEKSFORGEEKS
